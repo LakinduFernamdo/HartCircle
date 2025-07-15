7d7b30205f855d8affabcf4d07093479943192b3
2001lakindu <lakindu72001@gmail.com>
2025-07-15 19:45:21 +0530

    develop user account security functions

--- src/main/java/com/Project2/BackEnd/Config/SecurityConfig.java
+++ src/main/java/com/Project2/BackEnd/Config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll() // allow login
+                        .requestMatchers("/api/v1/**").permitAll() // allow login
                         .anyRequest().authenticated()               // secure all others
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
